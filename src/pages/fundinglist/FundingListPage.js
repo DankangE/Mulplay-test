6072fd0a2b797f01765662d5ef0ae4ef4b14d56a
DankangE <ekffy99@gmail.com>
2023-10-20 16:09:24 +0900

    펀딩 리스트 페이지

--- src/pages/fundinglist/FundingListPage.js
+++ src/pages/fundinglist/FundingListPage.js
@@ -1,6 +1,6 @@
 import "./FundingListPage.css"
 import { useState } from 'react';
-import image1 from '../../assets/image/profile1.png';
+import {image1, image2, image3, image4, image5, image6, image7, image8} from '../../assets/image/listpageicon/ListImg'
 import FundingListPage_Card from "../../components/layout/ListCard";
 
 
@@ -21,7 +21,7 @@ const FundingListPage = () => {
                     <li data-index="1">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image2} alt="" width={50}/>
                             </div>
                             <div>웹</div>
                         </button>
@@ -29,7 +29,7 @@ const FundingListPage = () => {
                     <li data-index="2">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image3} alt="" width={50}/>
                             </div>
                             <div>애플리케이션</div>
                         </button>
@@ -37,7 +37,7 @@ const FundingListPage = () => {
                     <li data-index="3">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image4} alt="" width={50}/>
                             </div>
                             <div>일반 소프트웨어</div>
                         </button>
@@ -45,7 +45,7 @@ const FundingListPage = () => {
                     <li data-index="4">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image5} alt="" width={50}/>
                             </div>
                             <div>게임</div>
                         </button>
@@ -53,7 +53,7 @@ const FundingListPage = () => {
                     <li data-index="5">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image6} alt="" width={50}/>
                             </div>
                             <div>커머스 쇼핑몰</div>
                         </button>
@@ -61,7 +61,7 @@ const FundingListPage = () => {
                     <li data-index="6">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image7} alt="" width={50}/>
                             </div>
                             <div>퍼블리싱</div>
                         </button>
@@ -69,7 +69,7 @@ const FundingListPage = () => {
                     <li data-index="7">
                         <button className="fundinglist_Imagetab">
                             <div className="Imagetab_img">
-                                <img src={image1} alt="" width={50}/>
+                                <img src={image8} alt="" width={50}/>
                             </div>
                             <div>디자인</div>
                         </button>
